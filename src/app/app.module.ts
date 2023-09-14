08b9042dfe1b81b8b1e4e7c782e37b40dd5ee928
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-09-14 22:38:45 +0300

    added resource component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ResourceComponent } from "./resource/resource.component";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResourceComponent
+    ResourceComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/resource/resource.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-resource',
+  templateUrl: './resource.component.html'
+})
+export class ResourceComponent {
+
+}
