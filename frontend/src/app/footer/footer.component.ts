f2a558306b8bc65517559e5fd13647d12201aa04
Iurii Golubnichenko <iurii.golubnichenko@gmail.com>
2023-10-20 20:54:45 +0300

    update footer component

--- frontend/src/app/footer/footer.component.ts
+++ frontend/src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {faEnvelope} from "@fortawesome/free-regular-svg-icons";
+import {faLinkedin, faSquareGithub, faStackOverflow} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
+  socialProfiles = [
+    { icon: faEnvelope, link: 'mailto:iurii.golubnichenko@gmail.com' },
+    { icon: faLinkedin, link: 'https://www.linkedin.com/in/iurii-golubnichenko/' },
+    { icon: faSquareGithub, link: 'https://github.com/Ghost-Rider-gu' },
+    { icon: faStackOverflow, link: 'https://stackoverflow.com/users/1599177/ghost-rider' }
+  ];
 }
